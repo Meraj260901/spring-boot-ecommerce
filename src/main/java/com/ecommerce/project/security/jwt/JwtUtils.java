e4bdd91ee944969b191492915dcbe0be3f55dead
Meraj Ahamed <merajahamed33345@gmail.com>
2026-08-15 23:39:59 +0530

    Implemented "/username", "/user" and "/signout" endpoints in AuthController class. Also removed Cookie value from getting it printed in console panel.

--- src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
+++ src/main/java/com/ecommerce/project/security/jwt/JwtUtils.java
@@ -38,7 +38,6 @@ public class JwtUtils {
     public String getJwtFromCookies(HttpServletRequest request){
         Cookie cookie = WebUtils.getCookie(request, jwtCookie);
         if (cookie != null){
-            System.out.println("COOKIE: " + cookie.getValue());
             return cookie.getValue();
         }else {
             return null;
@@ -51,6 +50,13 @@ public class JwtUtils {
                 .path("/api")
                 .maxAge(24 * 60 * 60)
                 .httpOnly(false)
+                .build();
+        return cookie;
+    }
+
+    public ResponseCookie getCleanJwtCookie(){
+        ResponseCookie cookie = ResponseCookie.from(jwtCookie, null)
+                .path("/api")
                 .build();
         return cookie;
     }
